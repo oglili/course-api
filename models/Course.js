9612da06b8cada6c9215bfdea09f72ad4b5ca2b0
oglili <osazuwaoghomwen@gmail.com>
2022-08-01 08:43:53 +0200

    models updated

--- models/Course.js
+++ models/Course.js
@@ -44,32 +44,3 @@ const Course = mongoose.model('Course', CourseSchema)
 
 module.exports = {Type, Univ, Course}
 
-
-/*const CourseSchema = new mongoose.Schema({
-    name: {
-        type:String,
-        required:[true, 'must provide a name'],
-        trim:true,
-        maxlength:[30, 'name cannot be more than 30 characters']
-    },
-    type: {
-        type:String,
-        required:[true, 'must provide a type of course'],
-        trim:true
-    },
-    university: {
-        type:String,
-        enum: {
-            values:['University of Vicenza','University of Verona','University of Brescia','University of Milano','University of Catania','University of Rome','University of Macerata'],
-            message:'{VALUE} is not supported'
-        },
-        trim:true
-    }
-
-},
-{
-    collation: { locale: 'en', strength: 2 }
-}
-)
-
-module.exports = mongoose.model('Course', CourseSchema)*/
